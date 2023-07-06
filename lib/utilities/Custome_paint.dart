9355c227f5d489d2c202f488ff16be53844e0847
oalshokri <janataanfor@gmail.com>
2023-07-06 17:00:58 +0300

    - add geolocator package

--- lib/utilities/Custome_paint.dart
+++ lib/utilities/Custome_paint.dart
@@ -1,56 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 
-class LoadingScreen extends StatefulWidget {
-  const LoadingScreen({super.key});
+class CustomePainterTest extends StatefulWidget {
+  const CustomePainterTest({super.key});
 
   @override
-  LoadingScreenState createState() => LoadingScreenState();
+  CustomePainterTestState createState() => CustomePainterTestState();
 }
 
-class LoadingScreenState extends State<LoadingScreen> {
-  /// Determine the current position of the device.
-  ///
-  /// When the location services are not enabled or permissions
-  /// are denied the `Future` will return an error.
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
-  }
-
+class CustomePainterTestState extends State<CustomePainterTest> {
+  //ToDo: 01 add getLocation fun
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,14 +17,18 @@ class LoadingScreenState extends State<LoadingScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: ElevatedButton(
-              onPressed: () async {
-                Position p = await _determinePosition();
-                print(p);
-              },
-              child: const Text('Get Location'),
+            child: CustomPaint(
+              size: Size(300, 170),
+              painter: MyPainter(30),
             ),
           ),
+
+          // ElevatedButton(
+          //   onPressed: () {
+          //     //ToDo: 02 Get the current location
+          //   },
+          //   child: const Text('Get Location'),
+          // ),
         ],
       ),
     );
